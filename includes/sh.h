e15abf5735871119c71ac049e6c4d6d485773aa1
Jere Rignell <jrignell@student.hive.fi>
2020-05-27 18:21:29 +0300

    Updated header file

--- includes/sh.h
+++ includes/sh.h
@@ -6,7 +6,7 @@
 /*   By: jrignell <jrignell@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/17 16:54:20 by eenasalorin       #+#    #+#             */
-/*   Updated: 2020/05/27 18:06:50 by jrignell         ###   ########.fr       */
+/*   Updated: 2020/05/27 18:20:06 by jrignell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,34 @@
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <signal.h>
+# include <termios.h>
 
 # define BSLASH 92
 # define DQUOTE 34
 # define SQUOTE 39
+# define LEFT 0x445B1B
+# define RIGHT 0x435B1B
+# define DOWN 0x425B1B
+# define UP 0x415B1B
+# define ENTER 0xA
+# define ESC 0x1B
+# define DEL 0x7E335B1B
+# define SPACE 0x20
+# define BSPACE 0x7F
+# define ASTERISK 0x2A
+# define MINUS 0x2D
+
+typedef struct termios	t_termios;
 
 typedef struct	s_sh
 {
 	char		**env;
 	char		**args;
+	t_termios	original_term_mode;
+	t_termios	current_term_mode;
 }				t_sh;
+
+t_sh			*g_shell;
 
 void			sh_loop(t_sh *sh);
 void			sh_cd(t_sh *sh);
